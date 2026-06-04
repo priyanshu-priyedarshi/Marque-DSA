a261f9ba4d6f39c76de6c19e4a4791c74024e186
Priyanshu Priyedarshi <daspriyanshu360@gmail.com>
2026-06-04 09:56:55 +0530

    Add files via upload

---
+++ Functions/PrimeinRange.java
@@ -0,0 +1,29 @@
+public class PrimeinRange {
+
+    public static boolean isPrime(int n) {
+        if (n == 2) {
+            return true;
+        }
+
+        for (int i = 2; i <= n - 1; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void primesInRange(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.println(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        primesInRange(20);
+    }
+}
